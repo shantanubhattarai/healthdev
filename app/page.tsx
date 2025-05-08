e42599b5fd3219b3a4b02adf8d57055732c7811f
Shantanu Bhattarai <shantanu.bhattarai271@gmail.com>
2025-05-08 10:07:23 +0545

    add a couple sections to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import HowWeHelpSection from "@/components/Home/HowWeHelpSection";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="px-4 lg:px-8">
+    <main className="px-4 lg:px-8 container mx-auto">
       <section className="text-background px-4 py-2 rounded-md flex flex-col gap-8 justify-center items-center min-h-96 bg-green-900 bg-noise bg-blend-soft-light text-center">
         <div className="max-w-xl">
           <h2 className="text-4xl tracking-wide font-serif font-medium mb-4">
@@ -16,15 +19,56 @@ export default function Home() {
         </button>
       </section>
 
-      <section className="py-12">
-        <h3 className="text-lg font-medium">Navigating Recovery Together</h3>
-        <p className="text-zinc-700">Let us join you in your recovery.</p>
-        <p className="text-zinc-700">
-          Engage in opportunities you choose. You get an inclusive environment,
-          surrounded by our values to create the experience that lets you put
-          your foot forward.
-        </p>
+      <section className="py-24">
+        <div className="flex gap-4 justify-center items-center">
+          <div className="flex flex-col gap-6">
+            <div>
+              <h3 className="text-4xl leading-14 mb-2">
+                Navigating Recovery Together
+              </h3>
+              <p className="text-zinc-700 text-xl">
+                Let us join you in your recovery.
+              </p>
+            </div>
+            <p className="text-zinc-700 text-xl max-w-xl">
+              Engage in opportunities you choose. You get an inclusive
+              environment, surrounded by our values to create the experience
+              that lets you put your foot forward.
+            </p>
+          </div>
+          <div className="relative w-96 h-96">
+            <Image
+              src="https://picsum.photos/1000/1000"
+              alt="home-1"
+              className="absolute inset-0 object-contain rounded-xl z-20 border-background border-4"
+              width={480}
+              height={480}
+            />
+            <Image
+              src="https://picsum.photos/800/800"
+              alt="home-1"
+              className="absolute -top-12 -left-6 object-contain rounded-full border-background border-8 z-30"
+              width={100}
+              height={100}
+            />
+            <Image
+              src="https://picsum.photos/1024/1024"
+              alt="home-1"
+              className="absolute top-4 -right-18 object-contain rounded-full border-background border-6 z-30"
+              width={100}
+              height={100}
+            />
+            <Image
+              src="https://picsum.photos/640/640"
+              alt="home-1"
+              className="absolute -top-12 -right-24 object-contain rounded-full z-10"
+              width={64}
+              height={64}
+            />
+          </div>
+        </div>
       </section>
+      <HowWeHelpSection />
     </main>
   );
 }
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,19 +2,28 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex justify-between px-4 lg:px-8 py-2 items-center">
-      <Link href="/" className="font-semibold text-lg py-2">
+    <header className="flex justify-between px-4 lg:px-8 py-4 items-center container mx-auto">
+      <Link href="/" className="font-semibold text-lg py-1">
         HealthDev
       </Link>
       <nav>
-        <ul className="flex">
-          <Link className="px-4 py-2 font-medium" href="/">
+        <ul className="flex gap-2">
+          <Link
+            className="px-2 py-1 font-medium border-b-2 border-transparent hover:border-green-800 transition-all"
+            href="/"
+          >
             About
           </Link>
-          <Link className="px-4 py-2 font-medium" href="/">
+          <Link
+            className="px-2 py-1 font-medium border-b-2 border-transparent hover:border-green-800 transition-all"
+            href="/"
+          >
             Services
           </Link>
-          <Link className="px-4 py-2 font-medium" href="/">
+          <Link
+            className="px-2 py-1 font-medium border-b-2 border-transparent hover:border-green-800 transition-all"
+            href="/"
+          >
             Contact
           </Link>
         </ul>
