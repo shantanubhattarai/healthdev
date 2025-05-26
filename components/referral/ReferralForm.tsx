3433492aee4cd073dddb2f2974cae831d2f6de68
Shantanu Bhattarai <shantanu.bhattarai271@gmail.com>
2025-05-26 06:46:00 +0545

    referral form + brand color

--- components/referral/ReferralForm.tsx
+++ components/referral/ReferralForm.tsx
@@ -5,7 +5,6 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { useState } from "react";
 import { format } from "date-fns";
 import { Calendar } from "../ui/calendar";
-import Select from "react-select";
 
 const interestedServices = [
   {
@@ -51,32 +50,34 @@ const ReferralForm = () => {
 
   return (
     <form className="flex flex-col gap-4">
-      <div>
-        <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
-          Date
-        </label>
-        <Popover>
-          <PopoverTrigger asChild>
-            <button className="border border-zinc-300 rounded-sm p-2 flex items-center justify-start gap-2 min-w-[240px]">
-              <CalendarIcon size={16} className="text-zinc-500" />
-              {date ? (
-                format(date, "PPP")
-              ) : (
-                <span className="text-zinc-500">Pick a date</span>
-              )}
-            </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0">
-            <Calendar
-              mode="single"
-              selected={date}
-              onSelect={setDate}
-              initialFocus
-            />
-          </PopoverContent>
-        </Popover>
+      <div className="grid grid-cols-2 gap-x-4">
+        <div>
+          <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+            Date
+          </label>
+          <Popover>
+            <PopoverTrigger asChild>
+              <button className="border border-zinc-300 rounded-sm p-2 flex items-center justify-start gap-2 w-full">
+                <CalendarIcon size={16} className="text-zinc-500" />
+                {date ? (
+                  format(date, "PPP")
+                ) : (
+                  <span className="text-zinc-500">Pick a date</span>
+                )}
+              </button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
       </div>
-      <section className="flex flex-col gap-4">
+      <section className="flex flex-col gap-4 mt-4">
         <h4 className="border-b pb-2">Referrer Details</h4>
         <div>
           <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
@@ -96,37 +97,49 @@ const ReferralForm = () => {
             placeholder="Your organization"
           />
         </div>
-        <div>
-          <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
-            Phone Number
-          </label>
-          <input
-            className="border border-zinc-300 rounded-sm p-2 block w-full"
-            placeholder="Your phone number"
-          />
-        </div>
-        <div>
-          <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
-            Email
-          </label>
-          <input
-            className="border border-zinc-300 rounded-sm p-2 block w-full"
-            placeholder="Your email address"
-          />
+        <div className="grid grid-cols-2 gap-x-4">
+          <div>
+            <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+              Phone Number
+            </label>
+            <input
+              className="border border-zinc-300 rounded-sm p-2 block w-full"
+              placeholder="Your phone number"
+            />
+          </div>
+          <div>
+            <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+              Email
+            </label>
+            <input
+              className="border border-zinc-300 rounded-sm p-2 block w-full"
+              placeholder="Your email address"
+            />
+          </div>
         </div>
         <div>
           <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
             What services are you interested in?
           </label>
-          <Select
-            isMulti
-            classNames={{ control: () => "!rounded-sm" }}
-            options={interestedServices}
-            instanceId={`interested-service`}
-          />
+          <ul>
+            {interestedServices.map((item) => (
+              <li
+                key={`interested-service-${item.value}`}
+                className="flex items-center space-x-2"
+              >
+                <input type="checkbox" id={`check-${item.value}`} />
+                <label
+                  htmlFor={`check-${item.value}`}
+                  className="text-zinc-700"
+                >
+                  {item.label}
+                </label>
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
-      <section className="flex flex-col gap-4">
+      <section className="flex flex-col gap-4 mt-4">
         <h4 className="border-b pb-2">Participant Details</h4>
         <div>
           <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
@@ -137,46 +150,215 @@ const ReferralForm = () => {
             placeholder="Your name"
           />
         </div>
+        <div className="grid grid-cols-2 gap-x-4">
+          <div>
+            <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+              Date of Birth
+            </label>
+            <Popover>
+              <PopoverTrigger asChild>
+                <button className="border border-zinc-300 rounded-sm p-2 flex items-center justify-start gap-2 w-full">
+                  <CalendarIcon size={16} className="text-zinc-500" />
+                  {date ? (
+                    format(date, "PPP")
+                  ) : (
+                    <span className="text-zinc-500">Pick a date</span>
+                  )}
+                </button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0">
+                <Calendar
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div>
+            <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+              Gender
+            </label>
+            <div className="flex space-x-2 wrap">
+              <div className="flex space-x-1 items-center">
+                <input
+                  className="border border-zinc-300"
+                  type="radio"
+                  id="gender-male"
+                  name="gender"
+                />
+                <label htmlFor="gender-male" className="text-zinc-700">
+                  Male
+                </label>
+              </div>
+              <div className="flex space-x-1 items-center">
+                <input
+                  className="border border-zinc-300"
+                  type="radio"
+                  id="gender-female"
+                  name="gender"
+                />
+                <label htmlFor="gender-female" className="text-zinc-700">
+                  Female
+                </label>
+              </div>
+              <div className="flex space-x-1 items-center grow">
+                <input
+                  className="border border-zinc-300"
+                  type="radio"
+                  id="gender-unanswered"
+                  name="gender"
+                />
+                <label htmlFor="gender-unanswered" className="text-zinc-700">
+                  Prefer not to say
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-x-4">
+          <div>
+            <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+              Phone Number
+            </label>
+            <input
+              className="border border-zinc-300 rounded-sm p-2 block w-full"
+              placeholder="Your phone number"
+            />
+          </div>
+          <div>
+            <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+              Email
+            </label>
+            <input
+              className="border border-zinc-300 rounded-sm p-2 block w-full"
+              placeholder="Your email address"
+            />
+          </div>
+        </div>
         <div>
           <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
-            Organization
+            Address
           </label>
           <input
             className="border border-zinc-300 rounded-sm p-2 block w-full"
-            placeholder="Your organization"
+            placeholder="Your address"
           />
         </div>
         <div>
           <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
-            Email
+            Reason for referral
           </label>
           <input
             className="border border-zinc-300 rounded-sm p-2 block w-full"
-            placeholder="Your email address"
+            placeholder="What is your reason for referral?"
           />
         </div>
         <div>
           <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
-            Phone Number
+            What is your disability?
           </label>
           <input
             className="border border-zinc-300 rounded-sm p-2 block w-full"
-            placeholder="Your phone number"
+            placeholder="What is your disability?"
           />
         </div>
       </section>
-      <div>
-        <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
-          Message
-        </label>
-        <textarea
-          placeholder="Any additional information you would like to send"
-          className="border border-zinc-300 rounded-sm p-2 block w-full resize-none"
-          rows={5}
-        />
-      </div>
+      <section className="flex flex-col gap-4">
+        <div>
+          <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+            Where did you hear about us?
+          </label>
+          <div className="flex space-x-2 wrap">
+            <div className="flex space-x-1 items-center">
+              <input
+                className="border border-zinc-300"
+                type="radio"
+                id="heard-google"
+                name="heard"
+              />
+              <label htmlFor="heard-google" className="text-zinc-700">
+                Google
+              </label>
+            </div>
+            <div className="flex space-x-1 items-center">
+              <input
+                className="border border-zinc-300"
+                type="radio"
+                id="heard-facebook"
+                name="heard"
+              />
+              <label htmlFor="heard-facebook" className="text-zinc-700">
+                Facebook
+              </label>
+            </div>
+            <div className="flex space-x-1 items-center">
+              <input
+                className="border border-zinc-300"
+                type="radio"
+                id="heard-instagram"
+                name="heard"
+              />
+              <label htmlFor="heard-instagram" className="text-zinc-700">
+                Instagram
+              </label>
+            </div>
+            <div className="flex space-x-1 items-center">
+              <input
+                className="border border-zinc-300"
+                type="radio"
+                id="heard-colleague"
+                name="heard"
+              />
+              <label htmlFor="heard-colleague" className="text-zinc-700">
+                Colleague or Friend
+              </label>
+            </div>
+          </div>
+        </div>
+        <div>
+          <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+            Enquiry
+          </label>
+          <textarea
+            placeholder="Any additional information you would like to send"
+            className="border border-zinc-300 rounded-sm p-2 block w-full resize-none"
+            rows={5}
+          />
+        </div>
+        <div>
+          <label className="block mb-1 text-zinc-600 text-sm tracking-wide">
+            Who should we contact?
+          </label>
+          <div className="flex space-x-2 wrap">
+            <div className="flex space-x-1 items-center">
+              <input
+                className="border border-zinc-300"
+                type="radio"
+                id="contact-participant"
+                name="contact-who"
+              />
+              <label htmlFor="contact-participant" className="text-zinc-700">
+                Contact participant directly
+              </label>
+            </div>
+            <div className="flex space-x-1 items-center">
+              <input
+                className="border border-zinc-300"
+                type="radio"
+                id="contact-referrer"
+                name="contact-who"
+              />
+              <label htmlFor="contact-referrer" className="text-zinc-700">
+                Contact referrer
+              </label>
+            </div>
+          </div>
+        </div>
+      </section>
       <button
-        className="text-medium bg-amber-800 bg-noise bg-blend-color-burn text-white px-6 py-2 rounded-sm tracking-wide text-sm cursor-pointer ring ring-amber-800 hover:ring-amber-600 transition-all"
+        className="text-medium bg-brand bg-noise bg-blend-color-burn text-white px-6 py-2 rounded-sm tracking-wide text-sm cursor-pointer ring ring-brand-secondary hover:bg-brand-secondary active:bg-brand transition-all"
         type="submit"
       >
         Send Message
